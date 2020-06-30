8f879dc6fdf8539ae592d7c1885686cb9c2f4a73
zhonglq <longquankey@126.com>
2020-06-30 19:34:54 +0800

    vuex getters

--- 13-Vuex/01-Vuex的基本使用/01-vuex/src/App.vue
+++ 13-Vuex/01-Vuex的基本使用/01-vuex/src/App.vue
@@ -4,6 +4,17 @@
     <button @click="add">+</button>
     <button @click="sub">-</button>
     <hello-world></hello-world>
+
+    <h3>显示学生喽</h3>
+    <h3>{{ this.$store.state.students }}</h3>
+    <h3>显示年龄大于40的</h3>
+    <h3>
+      {{ this.$store.getters.ageMore40 }}
+      <br />
+      length:{{ this.$store.getters.ageMore40Length }}
+    </h3>
+    <h3>根据ID筛选学生</h3>
+    {{ $store.getters.getStudentById(4) }}
   </div>
 </template>
 
--- 13-Vuex/01-Vuex的基本使用/01-vuex/src/store/index.js
+++ 13-Vuex/01-Vuex的基本使用/01-vuex/src/store/index.js
@@ -6,7 +6,13 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        counter: 1000
+        counter: 1000,
+        students: [
+            { id: 1, name: '旺财', age: 12 },
+            { id: 2, name: '小强', age: 31 },
+            { id: 3, name: '大明', age: 45 },
+            { id: 4, name: '狗蛋', age: 78 }
+        ]
     },
     mutations: {
         increament(state) {
@@ -14,6 +20,22 @@ const store = new Vuex.Store({
         },
         decreament(state) {
             state.counter--
+        }
+    },
+    getters: {
+        ageMore40(state) {
+            return state.students.filter(s => s.age > 40);
+        },
+        ageMore40Length(state, getters) {
+            return getters.ageMore40.length
+        },
+        getStudentById(state){
+            // return function(id){
+            //     return state.students.filter(s=>s.id==id)
+            // }
+            return id=>{ 
+                return state.students.filter(s=>s.id==id)
+            }
         }
     }
 })
